017f627eec7edc3cd28b35cc671e8282cc8909c9
cgrade <gradeisageek@gmail.com>
2023-08-01 02:00:30 +0100

    pep8 pycodestyle convention fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A script that returns Pascal triangle for n number"""
 
+
 def pascal_triangle(n):
     """A functionn that returns Pascal triangle with n rows"""
     outterList = []
@@ -8,11 +9,12 @@ def pascal_triangle(n):
     for eachRow in range(n):
         # initializing the inner list that rep. the columns
         innerList = []
-        
         for eachCol in range(eachRow + 1):
             if eachCol == 0 or eachCol == eachRow:
                 innerList.append(1)
             else:
-                innerList.append(outterList[eachRow -1][eachCol - 1] + outterList[eachRow - 1][eachCol])
+                innerList.append(
+                    outterList[eachRow - 1][eachCol - 1] +
+                    outterList[eachRow - 1][eachCol])
         outterList.append(innerList)
-    return(outterList)
+    return (outterList)
