eb6ac910d298ac1af525aa80cae1fb70fbe2ae35
cgrade <gradeisageek@gmail.com>
2023-08-01 01:35:55 +0100

    code refactored

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-This Module contain algorithm to construct a pascal triangle
-from the given input 'n' by a user
-where 'n' is the number of rows in the pascal triangle
-"""
+""" A script that returns Pascal triangle for n number"""
 
 def pascal_triangle(n):
     """A functionn that returns Pascal triangle with n rows"""
-
     outterList = []
+
     for eachRow in range(n):
         # initializing the inner list that rep. the columns
         innerList = []
+        
         for eachCol in range(eachRow + 1):
             if eachCol == 0 or eachCol == eachRow:
                 innerList.append(1)
